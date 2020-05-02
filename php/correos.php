1c3043b154f911c3612c4b9d185e7c7f418d3ec0
carlmrx <43156603+carlmrx@users.noreply.github.com>
2020-05-02 17:27:01 -0500

    correo 100% funcional

--- php/correos.php
+++ php/correos.php
@@ -1,10 +1,13 @@
 <?php
 $para      = $correo;
 $titulo    = 'Acuse de reporte sinatec';
-$mensaje   = "Hola".$nombre ."hemos recibido la informacion de tu problema, estamos trabajando para solucionarlo en breve recibiras una respuesta de parte de nuestro esquipo"."\r\n"."telrfono:01800 111222333444";
-$cabeceras = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
+$mensaje   = "Hola ".$nombre ." hemos recibido la informacion de tu problema, estamos trabajando para solucionarlo en breve recibiras una respuesta de nuestro equipo"."\r\n"."Para mas informes"."\r\n"."telefono:01800 111222333444";
+$cabeceras = 'From: sinatec-ayuda@gob.mx' . "\r\n" .//remitente que aparecera
+    'Reply-To: ca.serr.123@gmail.com' . "\r\n" .// a quien se le puede contestar
     'X-Mailer: PHP/' . phpversion();
+    $mail=@mail($para, $titulo, $mensaje, $cabeceras);//cuerpo de el correo
+    //copia
+    $para2="ca.serr.123@gmail.com";
+    $mail2=@mail($para2, $titulo, $mensaje, $cabeceras);
 
-mail($para, $titulo, $mensaje, $cabeceras);
 ?>
